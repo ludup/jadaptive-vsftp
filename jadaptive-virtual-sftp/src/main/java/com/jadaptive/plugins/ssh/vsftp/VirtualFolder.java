a4a1ded7b5474deaacc093f878da050df47700be
Lee Painter <lee@javassh.com>
2020-02-02 18:25:56 +0000

    Refactored permissions to use system registration, and optional tenant permissions. Now uses our own ExtensionFinder that scans classpath for @Extension annotations.

--- jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/VirtualFolder.java
+++ jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/VirtualFolder.java
@@ -2,17 +2,17 @@ package com.jadaptive.plugins.ssh.vsftp;
 
 import com.jadaptive.api.entity.EntityType;
 import com.jadaptive.api.repository.AssignableUUIDEntity;
-import com.jadaptive.api.template.JEntity;
-import com.jadaptive.api.template.JField;
+import com.jadaptive.api.template.Entity;
+import com.jadaptive.api.template.Member;
 import com.jadaptive.api.template.FieldType;
 
-@JEntity(name="Virtual Folder", resourceKey = "vfolder", type = EntityType.COLLECTION)
+@Entity(name="Virtual Folder", resourceKey = "vfolder", type = EntityType.COLLECTION)
 public class VirtualFolder extends AssignableUUIDEntity {
 
-	@JField(name="Mount Path", description = "The path on which this folder is mounted within the virtual file system", type = FieldType.TEXT)
+	@Member(name="Mount Path", description = "The path on which this folder is mounted within the virtual file system", type = FieldType.TEXT)
 	String mountPath;
 	
-	@JField(name = "Destination URI", description = "The destination URI serving as the source for this virutal folder", type = FieldType.TEXT)
+	@Member(name = "Destination URI", description = "The destination URI serving as the source for this virutal folder", type = FieldType.TEXT)
 	String destinationUri;
 
 	public String getMountPath() {
--- jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/commands/Unmount.java
+++ jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/commands/Unmount.java
@@ -2,10 +2,7 @@ package com.jadaptive.plugins.ssh.vsftp.commands;
 
 import java.io.IOException;
 
-import com.jadaptive.api.sshd.commands.AbstractTenantAwareCommand;
-import com.sshtools.common.files.vfs.VirtualFileFactory;
 import com.sshtools.common.permissions.PermissionDeniedException;
-import com.sshtools.common.policy.FileSystemPolicy;
 import com.sshtools.server.vsession.UsageException;
 import com.sshtools.server.vsession.VirtualConsole;
 
