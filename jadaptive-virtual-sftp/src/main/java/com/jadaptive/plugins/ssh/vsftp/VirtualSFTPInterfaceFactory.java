529fea918d531a0cf18b4dfe4f8707d35c017181
Brett Smith <brett@logonbox.com>
2023-06-02 00:28:10 +0100

    Use latest synergy API

--- jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/VirtualSFTPInterfaceFactory.java
+++ jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/VirtualSFTPInterfaceFactory.java
@@ -25,8 +25,7 @@ import com.sshtools.common.policy.FileFactory;
 import com.sshtools.common.policy.FileSystemPolicy;
 import com.sshtools.common.publickey.InvalidPassphraseException;
 import com.sshtools.common.publickey.SshKeyPairGenerator;
-import com.sshtools.common.sftp.extensions.DefaultSftpExtensionFactory;
-import com.sshtools.common.sftp.extensions.SupportedSftpExtensions;
+import com.sshtools.common.sftp.extensions.SftpExtensionLoaderFactory;
 import com.sshtools.common.ssh.SshConnection;
 import com.sshtools.common.ssh.SshException;
 import com.sshtools.server.SshServerContext;
@@ -86,15 +85,7 @@ public class VirtualSFTPInterfaceFactory implements SSHInterfaceFactory<SshServe
 		
 		
 		ctx.getPolicy(FileSystemPolicy.class).getSFTPExtensionFactories().add(
-			    new DefaultSftpExtensionFactory(SupportedSftpExtensions.POSIX_RENAME,
-			      SupportedSftpExtensions.MD5_FILE_HASH,
-			      SupportedSftpExtensions.COPY_FILE,
-			      SupportedSftpExtensions.COPY_DATA,
-			      SupportedSftpExtensions.CHECK_FILE_HANDLE,
-			      SupportedSftpExtensions.CHECK_FILE_NAME,
-			      SupportedSftpExtensions.OPEN_DIRECTORY_WITH_FILTER,
-			      SupportedSftpExtensions.CREATE_MULTIPART_FILE,
-			      SupportedSftpExtensions.OPEN_PART_FILE));
+			    new SftpExtensionLoaderFactory());
 		
 		ctx.getPolicy(FileSystemPolicy.class).setFileFactory(new FileFactory() {
 
