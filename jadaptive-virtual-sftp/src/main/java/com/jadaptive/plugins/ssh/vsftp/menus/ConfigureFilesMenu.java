7e13b3f6ff950ab95271fe06f55478e46cdfb2d8
Lee Painter <lee@javassh.com>
2023-06-26 11:16:36 +0100

    Change parent menu constant.

--- jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/menus/ConfigureFilesMenu.java
+++ jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/menus/ConfigureFilesMenu.java
@@ -39,7 +39,7 @@ public class ConfigureFilesMenu implements ApplicationMenu {
 
 	@Override
 	public String getParent() {
-		return ApplicationMenuService.CONFIGURATION_MENU;
+		return ApplicationMenuService.ADMINISTRATION_MENU;
 	}
 
 	@Override
