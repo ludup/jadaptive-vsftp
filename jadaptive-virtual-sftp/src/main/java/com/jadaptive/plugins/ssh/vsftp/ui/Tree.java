a497c9db34d7897e2608595b34a06269f0fd82ca
Lee Painter <lee@ludup.com>
2024-09-01 15:41:34 +0100

    Redirect to correct URL if used as a home page.

--- jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/ui/Tree.java
+++ jadaptive-virtual-sftp/src/main/java/com/jadaptive/plugins/ssh/vsftp/ui/Tree.java
@@ -1,8 +1,12 @@
 package com.jadaptive.plugins.ssh.vsftp.ui;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.jsoup.nodes.Document;
 import org.pf4j.Extension;
@@ -11,8 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.jadaptive.api.servlet.Request;
 import com.jadaptive.api.session.SessionUtils;
 import com.jadaptive.api.ui.AuthenticatedPage;
+import com.jadaptive.api.ui.PageCache;
 import com.jadaptive.api.ui.PageDependencies;
 import com.jadaptive.api.ui.PageProcessors;
+import com.jadaptive.api.ui.PageRedirect;
 import com.jadaptive.api.ui.RequestPage;
 import com.jadaptive.api.ui.renderers.DropdownInput;
 
@@ -25,13 +31,37 @@ public class Tree extends AuthenticatedPage {
 	@Autowired
 	private SessionUtils sessionUtils;
 	
+	@Autowired
+	private PageCache pageCache;
+	
 	@Override
 	public String getUri() {
 		return "tree";
 	}
+	
+	
+
+	@Override
+	protected void beforeProcess(String uri, HttpServletRequest request, HttpServletResponse response)
+			throws FileNotFoundException {
+		
+		super.beforeProcess(uri, request, response);
+		
+		/**
+		 * This is needed because the page JS reads the URI and expects it
+		 * to be consistent. If the users home page is this page it messes
+		 * with that logic.
+		 */
+		if(uri.equals("/app/ui")) {
+			throw new PageRedirect(pageCache.resolvePage(Tree.class));
+		}
+	}
+
+
 
 	@Override
 	protected void generateAuthenticatedContent(Document document) throws IOException {
+	
 		super.generateAuthenticatedContent(document);
 		
 		sessionUtils.addContentSecurityPolicy(Request.response(), "style-src", "unsafe-inline");
